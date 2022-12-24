03c5e8084d090785f82556a8d7c97982ea6eb4ea
uditdesai <udit.desai3@gmail.com>
2022-12-24 13:00:01 -0500

    add logo and contact

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <div>Hello</div>
+    <main className={styles.main}>
+      <div className={styles.logo}>
+        <Image src="/logo.png" alt="The Network" width={1640} height={462} />
+      </div>
+
+      <div className={styles.contact}>
+        <a href="mailto:info@simonefilms.com">contact</a>
+      </div>
     </main>
   );
 }
