5d67ab6e04a7cb1f0f790f0af26dc064ddeaf6f8
uditdesai <udit.desai3@gmail.com>
2023-01-01 11:39:15 -0500

    add video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,60 +10,60 @@ const formatTime = (value: number): string => {
 };
 
 const Home: React.FC = () => {
-  const countdown = useRef<HTMLElement>(null);
+  // const countdown = useRef<HTMLElement>(null);
 
-  const countDownDate = new Date("Jan 1, 2023 00:00:00 GMT-0500").getTime();
+  // const countDownDate = new Date("Jan 1, 2023 00:00:00 GMT-0500").getTime();
   // change to false to enable countdown
-  const [isCompleted, setIsCompleted] = useState(false);
-  let intervalId = useRef<NodeJS.Timer>();
-  const [date, setDate] = useState({
-    day: "00",
-    hour: "00",
-    minute: "00",
-    second: "00",
-  });
+  // const [isCompleted, setIsCompleted] = useState(true);
+  // let intervalId = useRef<NodeJS.Timer>();
+  // const [date, setDate] = useState({
+  //   day: "00",
+  //   hour: "00",
+  //   minute: "00",
+  //   second: "00",
+  // });
 
-  const checkTime = () => {
-    const now = new Date().getTime();
+  // const checkTime = () => {
+  //   const now = new Date().getTime();
 
-    const distance = countDownDate - now;
+  //   const distance = countDownDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //   const hours = Math.floor(
+  //     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //   );
+  //   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    setDate({
-      day: formatTime(days),
-      hour: formatTime(hours),
-      minute: formatTime(minutes),
-      second: formatTime(seconds),
-    });
+  //   setDate({
+  //     day: formatTime(days),
+  //     hour: formatTime(hours),
+  //     minute: formatTime(minutes),
+  //     second: formatTime(seconds),
+  //   });
 
-    if (countdown.current) countdown.current.style.opacity = "1";
+  //   if (countdown.current) countdown.current.style.opacity = "1";
 
-    if (distance < 0) {
-      setIsCompleted(true);
-      stopCountdown();
-    }
-  };
+  //   if (distance < 0) {
+  //     setIsCompleted(true);
+  //     stopCountdown();
+  //   }
+  // };
 
-  useEffect(() => {
-    checkTime();
-    intervalId.current = setInterval(checkTime, 1000);
-  }, []);
+  // useEffect(() => {
+  //   checkTime();
+  //   intervalId.current = setInterval(checkTime, 1000);
+  // }, []);
 
-  const stopCountdown = () => {
-    clearInterval(intervalId.current);
-  };
+  // const stopCountdown = () => {
+  //   clearInterval(intervalId.current);
+  // };
 
-  useEffect(() => {
-    if (isCompleted) {
-      console.log("show youtube stream");
-    }
-  }, [isCompleted]);
+  // useEffect(() => {
+  //   if (isCompleted) {
+  //     console.log("show youtube stream");
+  //   }
+  // }, [isCompleted]);
 
   return (
     <main className={styles.main}>
@@ -78,10 +78,21 @@ const Home: React.FC = () => {
         ></a>
       </div>
 
-      <div>
-        <span ref={countdown} className={styles.countdown}>
+      <div className={styles.videoWrapper}>
+        {/* <span ref={countdown} className={styles.countdown}>
           {date.day}:{date.hour}:{date.minute}:{date.second}
-        </span>
+        </span> */}
+        <iframe
+          style={{
+            aspectRatio: "16 / 9",
+            width: "100%",
+          }}
+          src="https://www.youtube.com/embed/DJMpNe6UNgA"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
 
       <div className={styles.contact}>
